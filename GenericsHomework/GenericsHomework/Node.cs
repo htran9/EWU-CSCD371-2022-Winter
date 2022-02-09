51390ca1530d9115f19df1c57add5a0772e38037
Hieu Tran <htran9@ewu.edu>
2022-02-08 19:07:32 -0800

    program.

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework
+{
+    public class Node<TValue>
+    {
+        public TValue Value { get; set; }
+        public Node<TValue> Next { get; private set; }
+
+        public Node(TValue value)
+        {
+            Value = value;
+            Next = this;
+        }
+        public void Append(TValue value)
+        {
+            Next = new Node<TValue>(value);
+       
+        }
+
+
+        public override string? ToString()
+        {
+            if(Value is null)
+            {
+                throw new ArgumentNullException(nameof(Value));
+            }
+            return Value.ToString();
+        }
+    }
+}
--- GenericsHomework/GenericsHomework/Program.cs
+++ GenericsHomework/GenericsHomework/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace GenericsHomework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Node<string> newNode1 = new Node<string>("10");
+            Node<string> newNode2 = new Node<string>("5");
+            Console.WriteLine(newNode1.Value);
+        }
+    }
+}
