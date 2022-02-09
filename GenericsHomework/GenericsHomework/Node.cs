ab2e200840653fdded025c3c67cd21a88b451434
Hieu Tran <htran9@ewu.edu>
2022-02-09 01:09:14 -0800

    added Exists method Added Clear method Did some testing

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +8,7 @@ namespace GenericsHomework
 {
     public class Node<TValue>
     {
-        public TValue Value { get; set; }
+        public TValue? Value { get; set; }
         public Node<TValue> Next { get; private set; }
 
         public Node<TValue>? Head = null;
@@ -21,17 +21,48 @@ namespace GenericsHomework
         }
         public void Append(TValue value)
         {
-            if(Head is null || Tail is null)
+            if (Head is null || Tail is null)
             {
                 Head = this;
                 Tail = this;
+            }
+            bool keyExists = Exists(value);
+            if (keyExists == true)
+            {
+                throw new ArgumentException("Cannot add duplicate value");
             }
             Node<TValue> newNode = new Node<TValue>(value);
             Tail.Next = newNode;
             Tail = newNode;
             Tail.Next = Head;
         }
-
+        public void Clear()
+        {
+            if(Tail is null)
+            {
+                throw new NullReferenceException("Empty list");
+            }
+            Tail.Next = Tail;
+            
+        }
+        public Boolean Exists(TValue key)
+        {
+            if(Head is null)
+            {
+                return false;
+            }
+            Node<TValue> temp = Head;
+            do
+            {
+                if((temp.Value is not null) && (temp.Value.Equals(key)))
+                {
+                    return true;
+                }
+                temp = temp.Next;
+            }
+            while(temp != Head);
+            return false;
+        }
         public override string? ToString()
         {
             if(Value is null)
--- GenericsHomework/GenericsHomework/Program.cs
+++ GenericsHomework/GenericsHomework/Program.cs
@@ -7,19 +7,31 @@ namespace GenericsHomework
         public static void Main(string[] args)
         {
             Node<string> newNode1 = new Node<string>("10");
-            /*newNode1.Append("30");
+            newNode1.Append("30");
             newNode1.Append("50");
-            newNode1.Append("60");*/
+            newNode1.Append("60");
             //Node<string> newNode3 = newNode2.Append("50");
             //newNode1.Append("50");
-            //Console.WriteLine(newNode1.Value);
-            //Console.WriteLine(newNode1.Next);
+            Console.WriteLine(newNode1.Value);
+            Console.WriteLine(newNode1.Next);
+            Console.WriteLine(newNode1.Next.Next);
+            Console.WriteLine(newNode1.Next.Next.Next);
+            Console.WriteLine(newNode1.Next.Next.Next.Next);
+
             //Console.WriteLine(newNode2.Next);
             //Console.WriteLine(newNode3.Value);
             //Console.WriteLine(newNode3.Next);
             //Console.WriteLine(newNode1.Next);
             //Console.WriteLine(newNode2.Next);
-            Console.WriteLine(newNode1.Next);
+            /* bool temp = newNode1.Exists("35");
+             if(temp == true)
+             {
+                 Console.WriteLine("KEY FOUNDDD");
+             }
+             else
+             {
+                 Console.WriteLine("KEY NOT FOUND");
+             }*/
         }
     }
 }
