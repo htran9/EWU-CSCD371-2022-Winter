65739e69e2aa4af0afa269601fd401fbe8909d7e
DakotaCondos <dakota242424@live.com>
2022-02-09 12:13:40 -0800

    Changed some things

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,78 +1,57 @@
-﻿namespace GenericsHomework
+﻿namespace GenericsHomework;
+public class Node<TValue>
 {
-    public class Node<TValue>
+    public TValue? Value { get; set; }
+    public Node<TValue> Next { get; private set; }
+    public Node<TValue> Root;
+
+    public Node(TValue value)
     {
-        public TValue? Value { get; set; }
-        public Node<TValue> Next { get; private set; }
-
-        public Node<TValue>? Head = null;
-        public Node<TValue>? Tail = null;
-        public Node<TValue>? Root = null;
-        public int anint = 10;
-
-        public Node(TValue value)
-        {
-            Value = value;
-            Next = this;
-            Root = this;
-        }
-        public void Append(TValue value)
-        {
-            if (Head is null || Tail is null)
-            {
-                Head = this;
-                Tail = this;
-            }
-            bool keyExists = Exists(value);
-            if (keyExists == true)
-            {
-                throw new ArgumentException("Cannot add duplicate value");
-            }
-            Node<TValue> newNode = new Node<TValue>(value);
-            Tail.Next = newNode;
-            Tail = newNode;
-            Tail.Next = Head;
-        }
-        public void Clear()
-        {
-            if (Tail is null)
-            {
-                throw new NullReferenceException("Empty list");
-            }
-            Tail.Next = Tail;
-
-        }
-        public Boolean Exists(TValue key)
-        {
-            if (Root is not null && key is not null)
-            {
-                Node<TValue> currentNode = Root;
-                do
-                {
-                    if (key.Equals(currentNode.Value))
-                        return true;
-                } while (currentNode.Next != Root);
-            }
-            return false;
-        }
-
-
-        //if(Head is null)
-        //{
-        //    return false;
-        //}
-        //Node<TValue> temp = Head;
-        //do
-        //{
-        //    if((temp.Value is not null) && (temp.Value.Equals(key)))
-        //    {
-        //        return true;
-        //    }
-        //    temp = temp.Next;
-        //}
-        //while(temp != Head);
-        //return false;
+        Value = value;
+        Next = this;
+        Root = this;
     }
+    public void Append(TValue value)
+    {
+
+        bool keyExists = Exists(value);
+        if (keyExists == true)
+        {
+            throw new ArgumentException("Cannot add duplicate value");
+        }
+
+        Node<TValue> newNode = new Node<TValue>(value);
+
+    }
+
+    private Node<TValue> GetLast()
+    {
+        Node<TValue> currentNode = Root;
+        while(currentNode.Next != Root)
+        {
+            currentNode = currentNode.Next;
+        }
+        return currentNode;
+    }
+    public void Clear()
+    {
+        Root.Next = this;
+
+    }
+    public Boolean Exists(TValue key)
+    {
+        if (Root is not null && key is not null)
+        {
+            Node<TValue> currentNode = Root;
+            do
+            {
+                if (key.Equals(currentNode.Value))
+                    return true;
+            } while (currentNode.Next != Root);
+        }
+        return false;
+    }
+
     public override string? ToString()
     {
         if (Value is null)
@@ -82,4 +61,5 @@
         return Value.ToString();
     }
 }
-}
+
+
