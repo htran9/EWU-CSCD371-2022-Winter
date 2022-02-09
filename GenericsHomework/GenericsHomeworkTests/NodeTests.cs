2b5f03cb1b089a620315bced4b584745ff912de1
DakotaCondos <dakota242424@live.com>
2022-02-09 11:17:14 -0800

    added testclass

---
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsHomeworkTests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Node<string> newNode1 = new Node<string>("10");
+    }
+}
