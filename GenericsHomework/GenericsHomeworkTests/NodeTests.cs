7f88d383e9fe626ab6067606c40116975288c420
DakotaCondos <dakota242424@live.com>
2022-02-10 11:30:09 -0800

    added unit test

--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -7,17 +7,23 @@ namespace GenericsHomeworkTests;
 public class NodeTests
 {
     [TestMethod]
-    public void NodeTypeString_Value_test()
+    public void NodeTypeString_Value_Equals_test()
     {
         Node<string> newNode = new ("test");
         if (newNode is not null && newNode.Value is not null)
         {
             Assert.AreEqual<string>("test", newNode.Value);
         }
+
+        Node<string> newNode2 = new("test2", newNode);
+        if (newNode is not null && newNode2.Value is not null)
+        {
+            Assert.AreEqual<string>("test2", newNode2.Value);
+        }
     }
 
     [TestMethod]
-    public void MyTestMethod()
+    public void NodeSetsRootToSelf_True()
     {
 
     }
