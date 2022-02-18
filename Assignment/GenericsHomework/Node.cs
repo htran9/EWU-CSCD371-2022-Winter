bed53be834a7a0232014b3bc0cd0a070d05cf83d
DakotaCondos <79940799+DakotaCondos@users.noreply.github.com>
2022-02-18 08:23:53 -0800

    added new projects for node and nodeTests then copied data from previous assignment to files

---
+++ Assignment/GenericsHomework/Node.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+
+namespace GenericsHomework;
+public class Node<TValue> : IEnumerable<Node<TValue>>
+{
+    public TValue Value { get; }
+    public Node<TValue> Next { get; private set; }
+    public Node<TValue> Root;
+
+    public Node(TValue value)
+    {
+        Value = value;
+        Next = this;
+        Root = this;
+    }
+    public Node(TValue value, Node<TValue> node)
+    {
+        Value = value;
+        Next = node;
+        Root = this;
+    }
+    public void Append(TValue value)
+    {
+        bool keyExists = Exists(value);
+        if (keyExists == true)
+        {
+            throw new ArgumentException("Cannot add duplicate value");
+        }
+        Node<TValue> lastNode = GetLast();
+        lastNode.Next = new Node<TValue>(value, Root);
+    }
+
+    public Node<TValue> GetLast()
+    {
+        Node<TValue> currentNode = Root;
+        while (currentNode.Next != Root)
+        {
+            currentNode = currentNode.Next;
+        }
+        return currentNode;
+    }
+    public void Clear()
+    {
+        // The garbage collecter picks up objects once they are no longer referenced,
+        // so removing the 'Next' reference to a node would allow the garbage collector to
+        // delete it, and subsequently delete the node it WAS pointing to because that
+        // node no longer has a reference to it and etc...
+        // 
+        // Removing a Node's Next reference effectively removes all items from that Node's list except
+        // the current node itself, because the list is now only pointing to a single node circularly (itself)
+        Root.Next = this;
+    }
+    public Boolean Exists(TValue key)
+    {
+        Node<TValue> currentNode = Root;
+        do
+        {
+            if (key is null)
+            {
+                if (currentNode.Value is null)
+                    return true;
+            }
+            else if (key.Equals(currentNode.Value))
+                return true;
+            currentNode = currentNode.Next;
+        } while (currentNode != Root);
+
+        return false;
+    }
+
+    public override string? ToString()
+    {
+        return Convert.ToString(Value);
+    }
+
+    public IEnumerable<TValue> ChildItems(int maximum) // i think this is what is needed for assignment5+6?
+    {
+        List<TValue> children = new();
+        Node<TValue> currentNode = Root;
+
+        int counter = 1;
+        do
+        {
+            children.Add(currentNode.Value);
+            counter++;
+            currentNode = currentNode.Next;
+        } while (currentNode != Root && counter < maximum);
+        return children;
+    }
+
+    public IEnumerator<Node<TValue>> GetEnumerator()
+    {
+        Node<TValue> current = this;
+
+        do
+        {
+            yield return current;
+
+            current = current.Next;
+        } while (current != this);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+        // just call above method?
+        // call through cast, not a yeild return
+    }
+}
+
+
---
+++ Assignment/GenericsHomeworkTests/NodeTests.cs
@@ -0,0 +1,112 @@
+using GenericsHomework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GenericsHomeworkTests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void NodeTypeString_Value_Equals_test()
+    {
+        Node<string> newNode = new("test");
+        Assert.AreEqual<string>("test", newNode.Value);
+
+        Node<string> newNode2 = new("test2", newNode);
+        Assert.AreEqual<string>("test2", newNode2.Value);
+    }
+
+    [TestMethod]
+    public void NodeSetsRootToSelf_True()
+    {
+        Node<string> newNode = new("test");
+        Assert.IsTrue(newNode.Equals(newNode.Root));
+
+        Node<string> newNode2 = new("test2", newNode);
+        Assert.IsTrue(newNode2.Equals(newNode2.Root));
+    }
+
+    [TestMethod]
+    public void NodeExists_ReturnTrue()
+    {
+        Node<string> newNode = new("10");
+        Assert.IsTrue(newNode.Exists("10"));
+    }
+
+    [TestMethod]
+    public void NodeExists_ReturnFalse()
+    {
+        Node<string> newNode = new("10");
+        Assert.IsFalse(newNode.Exists("12"));
+    }
+
+    [TestMethod]
+    public void NodeExists_NullValue_ReturnTrue()
+    {
+        string empty = null!;
+        Node<string> newNode = new("start");
+        newNode.Append("middle");
+        newNode.Append(empty);
+        newNode.Append("end");
+
+        Assert.IsTrue(newNode.Exists(null!));
+    }
+
+    [TestMethod]
+    public void NodeAppend_DuplicateValue_throwsException()
+    {
+        _ = Assert.ThrowsException<ArgumentException>(() =>
+        {
+            Node<double> newNode = DoubleRadioStations();
+            newNode.Append(42.0);
+        });
+    }
+
+    [TestMethod]
+    public void NodeAppend_AppendedExists_ReturnsTrue()
+    {
+        Node<string> newNode = new("first");
+        newNode.Append("Second");
+        Assert.IsTrue(newNode.Exists("Second"));
+    }
+
+    [TestMethod]
+    public void NodeLast_ReturnLast()
+    {
+        Node<string> newNode = new("1");
+        newNode.Append("2");
+        newNode.Append("3");
+        Node<string> lastNode = newNode.GetLast();
+        Assert.AreEqual<string>("3", lastNode.ToString()!);
+        Assert.AreEqual<string>(newNode.ToString()!, lastNode.Next.ToString()!);
+    }
+
+    [TestMethod]
+    public void NodeClear_RemovesAllNonRootNodes()
+    {
+        Node<double> newNode = DoubleRadioStations();
+        Assert.IsTrue(newNode.Exists(42.0));
+        Assert.IsTrue(newNode.Exists(92.9));
+        Assert.IsTrue(newNode.Exists(94.5));
+        Assert.IsTrue(newNode.Exists(103.5));
+        Assert.IsTrue(newNode.Exists(105.7));
+
+        newNode.Clear();
+        Assert.IsTrue(newNode.Exists(42.0));
+        Assert.IsFalse(newNode.Exists(92.9));
+        Assert.IsFalse(newNode.Exists(94.5));
+        Assert.IsFalse(newNode.Exists(103.5));
+        Assert.IsFalse(newNode.Exists(105.7));
+    }
+
+    public static Node<double> DoubleRadioStations()
+    {
+        Node<double> newNode = new(42.0);
+        newNode.Append(92.9);
+        newNode.Append(94.5);
+        newNode.Append(103.5);
+        newNode.Append(105.7);
+        return newNode;
+    }
+}
