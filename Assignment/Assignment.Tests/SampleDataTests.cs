ca2ead34a1cf0ee06be2bb679ce997dd81e9addb
htran9 <htran9@ewu.edu>
2022-02-17 22:40:14 -0800

    unit tested people property.

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -8,24 +8,26 @@ namespace Assignment.Tests
     [TestClass]
     public class SampleDataTests
     {
+        SampleData sampleData = new SampleData();
+
         [TestMethod]
         public void CsvRows_GetData_NotNull()
         {
-            SampleData data = new SampleData();
-            Assert.IsNotNull(data.CsvRows);
+  
+            Assert.IsNotNull(sampleData.CsvRows);
         }
 
         [TestMethod]
         public void CsvRows_SkipFirst_ReturnCount()
         {
-            IEnumerable<string> temp = new SampleData().CsvRows;
+            IEnumerable<string> temp = sampleData.CsvRows;
             Assert.AreEqual(50, temp.Count());
         }
         [TestMethod]
         public void GetUniqueSortedListOfState_AscendingOrder_IsTrue()
         {
-            IEnumerable<string> temp = new SampleData().GetUniqueSortedListOfStatesGivenCsvRows();
-            IEnumerable<string> temp2 = new SampleData().CsvRows.Select(line => line.Split(',')).Select(x => x[6]).OrderBy(x => x).Distinct();
+            IEnumerable<string> temp = sampleData.GetUniqueSortedListOfStatesGivenCsvRows();
+            IEnumerable<string> temp2 = sampleData.CsvRows.Select(line => line.Split(',')).Select(x => x[6]).OrderBy(x => x).Distinct();
             Assert.IsTrue(temp.SequenceEqual(temp2));
 
         }
@@ -33,23 +35,34 @@ namespace Assignment.Tests
         public void GetAggregateSortedList_ConvertToString_Success()
         {
             string tempString = "AL,AZ,CA,DC,FL,GA,IN,KS,LA,MD,MN,MO,MT,NC,NE,NH,NV,NY,OR,PA,SC,TN,TX,UT,VA,WA,WV";
-            string actualString = new SampleData().GetAggregateSortedListOfStatesUsingCsvRows();
+            string actualString = sampleData.GetAggregateSortedListOfStatesUsingCsvRows();
             Assert.IsTrue(tempString.Equals(actualString));
         }
         [TestMethod]
         public void PeopleObjectCreated_NotNull()
         {
-            SampleData sampleData = new SampleData();
             Assert.IsNotNull(sampleData.People);
         }
         [TestMethod]
         public void PeopleObject_EqualTo_CsvRowsSorted_IsTrue()
         {
-            SampleData sampleData = new SampleData();
-            IEnumerable<string> temp = sampleData.CsvRows.OrderBy(state => state[6]).ThenBy(city => city[5]).ThenBy(zip => zip[7]);
-            IEnumerable<string> people = sampleData.People.Cast<string>();
+            IEnumerable<string> people = sampleData.People
+                .Select(items => $"{ items.FirstName },{ items.LastName },{ items.EmailAddress },{ items.Address.StreetAddress }" +
+                $",{ items.Address.City },{ items.Address.State },{ items.Address.Zip }");
 
-
+            IEnumerable<string> csvSorted = sampleData.CsvRows.Select(line => line.Split(','))
+                .Select(items => new {
+                    FirstName = items[1],
+                    LastName = items[2],
+                    Email = items[3],
+                    StreetAddress = items[4],
+                    City = items[5],
+                    State = items[6],
+                    ZipCode = items[7],
+                }).OrderBy(item => item.State).ThenBy(item => item.City).ThenBy(item => item.ZipCode)
+                .Select(items => $"{ items.FirstName },{ items.LastName },{ items.Email },{ items.StreetAddress }" +
+                $",{ items.City },{ items.State },{ items.ZipCode }");
+            Assert.IsTrue(people.SequenceEqual(csvSorted));
         }
         [TestMethod]
         public void People_FilterByEmail_ReturnFirstLastName()
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -14,17 +14,23 @@
             Predicate<string> filter = email;
             static bool email(string email) => email.Contains("jdaneltim@jimdo.com");
             IEnumerable<(string, string)> result = sampleData.FilterByEmailAddress(filter);
+           
             //Console.WriteLine(result.First());
             //Console.WriteLine(result.First());
             //Console.WriteLine(sampleData.GetAggregateSortedListOfStatesUsingCsvRows());
-            foreach (var item in people)
+            /*foreach (var item in people)
             {
                 Console.WriteLine(item.FirstName + "," + item.LastName + "," + item.EmailAddress + "," + item.Address.StreetAddress
                     + "," + item.Address.City + "," + item.Address.State + "," + item.Address.Zip);
-            }
+            }*/
             //List<string> tempList = sampleData.CsvRows.ToList();
             //Console.WriteLine(tempList[1]);
-            /*foreach (var item in people)
+           /* foreach (var item in people1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------------------------------------------------------");
+            foreach (var item in temptest)
             {
                 Console.WriteLine(item);
             }*/
@@ -37,7 +43,6 @@
         private readonly Lazy<IEnumerable<string>> _LazyCsvRows = new Lazy<IEnumerable<string>>(() => File.ReadLines(@"People.csv").Where(line => !string.IsNullOrWhiteSpace(line)).Skip(1).Select(line => line));
         public IEnumerable<string> CsvRows { get { return _LazyCsvRows.Value; }}
         // 2.
-        
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
         {
             return CsvRows.Select(line => line.Split(',')).Select(x => x[6]).OrderBy(x => x).Distinct();
@@ -53,8 +58,9 @@
         // 4.
         // -----Not yet tested-----. 
         public IEnumerable<IPerson> People => _LazyCsvRows.Value.Select(line => line.Split(',')).OrderBy(state => state[6]).ThenBy(city => city[5]).ThenBy(zip => zip[7])
-            .Select(person => new Person(person[1], person[2], new Address(person[4], person[5], person[6], person[7]), person[3])).ToList();
+            .Select(person => new Person(person[1], person[2], new Address(person[4], person[5], person[6], person[7]), person[3]));
 
+        
         // 5.
         public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
         {
