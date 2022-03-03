fc995e908d05e766ef3b37155e4c18eaa8b0e258
htran9 <htran9@ewu.edu>
2022-03-02 19:56:59 -0800

    added menu option.

--- Project/js/index.js
+++ Project/js/index.js
@@ -38,52 +38,17 @@ function writeJoke() {
   }
 }
 
-/* button.addEventListener("click", function() {
-  axios({
-    method: 'get',
-    url: 'https://geek-jokes.sameerkumar.website/api',
-    responseType: 'stream'
-  })
-    .then(sleeper(5000)).then(function(response) {
-      let joke = document.querySelector(".telljoke")
-      joke.innerText = response.data;
-    })}); */
-
-    button.addEventListener("click", writeJoke);
-
-
-
-    function sleeper(ms) {
-      return function(response) {
-        return new Promise(resolve => setTimeout(() => resolve(response), ms));
-      };
-    }
-
-/* 
-button.addEventListener("click", function() {
-  fetch('https://geek-jokes.sameerkumar.website/api').then(response => response.json())
-  .then(data => {
-    let joke = document.querySelector(".telljoke")
-    joke.innerHTML = data;
-  })
-}) */
-
-/* function myFunction() {
-  document.getElementsById("drop").classList.toggle("show");
+button.addEventListener("click", writeJoke);
+function sleeper(ms) {
+  return function(response) {
+    return new Promise(resolve => setTimeout(() => resolve(response), ms));
+  };
 }
-    
-window.onclick = function(event) {
-  if(!event.target.matches('menu')) {
-    var dropdowns - document.getElementsByClassName("dropdownoption");
-    for(var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+
+function showMenu() {
+  var drop = document.getElementById("drop");
+  drop.classList.toggle("hidden");
 }
-     */
     
     
 
