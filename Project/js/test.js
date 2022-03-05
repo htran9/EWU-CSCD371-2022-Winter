31b8dddad4c33ea115f32decba16bcb576b3dfc9
htran9 <htran9@ewu.edu>
2022-03-04 20:39:57 -0800

    added creativity to  all menu buttons.

--- Project/js/test.js
+++ Project/js/test.js
@@ -3,7 +3,7 @@ function myFunction() {
 }
   
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+/*   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
@@ -14,7 +14,7 @@ function myFunction() {
         }
       }
     }
-}
+} */
 
 var button = document.querySelector('.anotherjoke');
 writeJoke();
@@ -39,4 +39,35 @@ function sleeper(ms) {
   return function(response) {
     return new Promise(resolve => setTimeout(() => resolve(response), ms));
   };
+}
+
+// Extra Credit Button Three. Make screen disappear for 5 secs.
+var card = document.getElementById("buttonthree");
+function screenGone() {
+  card.style.visibility = "hidden";
+  setTimeout(screenBack(), 5000);
+}
+function screenBack() {
+  return function() {
+    card.style.visibility = "visible";
+  }
+}
+// Extra credit to make Card fade in and out when Button 4 is pressed.
+function Blink() {
+  var blink = document.getElementById("card");
+  blink.classList.toggle("blinking");
+}
+
+// Change the background color when Button 1 is clicked.
+function RandomButtonColor() {
+  let colors = ["#ffd0d2","#fffdd0","#d0fffd","#d0d2ff"];
+  document.getElementsByTagName('body')[0].style.background = "linear-gradient(90deg, "+colors[randomNumber(0,4)]+" "+"50%, "+colors[randomNumber(0,4)] + ")";
+}
+function randomNumber(min,max){
+  return Math.floor((Math.random() * max) + min);
+}
+// Make the button turn upside down.
+function Rotate() {
+  var rotate = document.getElementById("buttontwo");
+  rotate.classList.toggle("rotate");
 }
