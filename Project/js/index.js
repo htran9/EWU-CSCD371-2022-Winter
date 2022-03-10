8d6b683a13ac9db23ab9f49dc4ab0f4dc8e34193
DakotaCondos <dakota242424@live.com>
2022-03-10 12:44:28 -0800

    simplified joke delay

--- Project/js/index.js
+++ Project/js/index.js
@@ -1,95 +1,69 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
 var button = document.querySelector('.anotherjoke');
 let tellJoke = document.querySelector(".telljoke")
-let tellJoke2 = document.querySelector(".telljoke2")
-let isTwoPart = true;
-let onePartText = "default1";
-let twoPartText = "default2";
-
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+writeJoke();
+function writeJoke() {
+{
+  tellJoke.innerHTML = "Loading...";
+  fetch('https://v2.jokeapi.dev/joke/Programming').then(response => {
+    if (!response.ok) {
+        throw new Error();
+    }
+    return response.json();
+    }).then(sleeper(4000))
+    .then(data => {
+      if(data['type'] == "single") {
+        tellJoke.innerHTML = data['joke'];
+      }else {
+        tellJoke.innerHTML = data['setup'] + "<br><br>";
+        setTimeout(function() {
+          tellJoke.innerHTML += data['delivery'];
+        }, 4000)
+      }
+  })
+  .catch(function (error) {
+    tellJoke.innerText = "An error has occurred, please try again in a few moments";
+  });
 }
-
-function displayJoke() {
-  return function(response) {
-    return new Promise(resolve => {
-      if(isTwoPart){tellJoke2.innerHTML = twoPartText}
-      else{tellJoke2.innerHTML = ""}
-    });
-  };
-}
-
-function sleeper(ms) {
-  return function(response) {
-    return new Promise(resolve => setTimeout(() => resolve(response), ms));
-  };
 }
 
 button.addEventListener("click", writeJoke);
-
-writeJoke();
-function writeJoke() {
-  {
-    tellJoke.innerHTML = "Fetching Joke...";
-    tellJoke2.innerHTML = "";
-    fetch('https://v2.jokeapi.dev/joke/Programming').then(response => {
-      if (!response.ok) {
-          throw new Error();
-      }
-      return response.json();
-      }).then(sleeper(4000))
-      .then(data => {
-        if(data['type'] == "single") {
-          isTwoPart = false;
-          tellJoke.innerHTML = data['joke'];
-          tellJoke2.innerHTML =  "";
-          twoPartText = "";
-        }else{
-          isTwoPart = true;
-          tellJoke.innerHTML = data['setup'];
-          twoPartText = data.delivery;
-        }
-    })
-    .then(sleeper(4000))
-    .then(displayJoke())
-    .catch(function (error) {
-      tellJoke.innerText = "An error has occurred, please try again in a few moments";
-    });
-  }
+function sleeper(ms) {
+return function(response) {
+  return new Promise(resolve => setTimeout(() => resolve(response), ms));
+};
 }
 
 // Extra Credit Button Three. Make screen disappear for 5 secs.
 var card = document.getElementById("buttonthree");
-
 function screenGone() {
-  card.style.visibility = "hidden";
-  setTimeout(screenBack(), 5000);
+card.style.visibility = "hidden";
+setTimeout(screenBack(), 5000);
 }
-
 function screenBack() {
-  return function() {
-    card.style.visibility = "visible";
-  }
+return function() {
+  card.style.visibility = "visible";
 }
-
+}
 // Extra credit to make Card fade in and out when Button 4 is pressed.
 function magic() {
-  var blink = document.getElementById("card");
-  blink.classList.toggle("blinking");
+var blink = document.getElementById("card");
+blink.classList.toggle("blinking");
 }
 
 // Change the background color when Button 1 is clicked.
 function randomButtonColor() {
-  let colors = ["#ffd0d2","#fffdd0","#d0fffd","#d0d2ff"];
-  document.getElementsByTagName('body')[0].style.background = "linear-gradient(90deg, "+colors[randomNumber(0,4)]+" "+"50%, "+colors[randomNumber(0,4)] + ")";
+let colors = ["#ffd0d2","#fffdd0","#d0fffd","#d0d2ff"];
+document.getElementsByTagName('body')[0].style.background = "linear-gradient(90deg, "+colors[randomNumber(0,4)]+" "+"50%, "+colors[randomNumber(0,4)] + ")";
 }
-
 function randomNumber(min,max) {
-  return Math.floor((Math.random() * max) + min);
+return Math.floor((Math.random() * max) + min);
 }
-
 // Make the button turn upside down.
 function rotate() {
-  var rotate = document.getElementById("buttontwo");
-  rotate.classList.toggle("rotate");
+var rotate = document.getElementById("buttontwo");
+rotate.classList.toggle("rotate");
 }
-
